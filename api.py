7bb8c01b8ca7d7731d5f4766822d63fc0a172053
denislav <127776832+deteto747chudo@users.noreply.github.com>
2024-09-09 03:33:23 +0300

    Update api.py

--- api.py
+++ api.py
@@ -69,4 +69,4 @@ def delete_task(task_id):
     return jsonify({'result': 'Task Deleted!'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
